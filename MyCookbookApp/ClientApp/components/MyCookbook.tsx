d28215343145e262510d6dc915fc814c2662e1c6
cristianagrigoriu <cristiana.grigoriu@gmail.com>
2018-08-23 18:07:35 +0300

    Create initial react project
    With a MyCookbook tab

---
+++ MyCookbookApp/ClientApp/components/MyCookbook.tsx
@@ -0,0 +1,12 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import 'isomorphic-fetch';
+
+export class MyCookbook extends React.Component<RouteComponentProps<{}>, String> {
+    public render() {
+        return <p>My Cookbook</p>;
+    }
+}
